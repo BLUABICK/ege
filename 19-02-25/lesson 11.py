c4020e80ae8878124dc3db9e660acea04aca2f3d
AMishunov <mishunov612@gmail.com>
2025-02-19 17:50:09 +0200

    задачи со строками

---
+++ 19-02-25/lesson 11.py
@@ -0,0 +1,46 @@
+#lower()
+# pod = input()
+# bob = ''
+# for i in pod:
+#     if 'A' <= i <= 'Z':
+#         bob += chr(ord(i) + 32)
+#     else:
+#         bob += i
+# print(bob)
+
+#isalpha()
+# bukv = input()
+# bukv2 = True
+# for i in bukv:
+#     if '0' <= i <= '9':
+#         bukv2 = False
+#         break
+# print(bukv2)
+
+# text = input()
+# digit = ''
+# for i in text:
+#     if 'a' <= i <= 'z' or 'A' <= i <= 'Z':
+#         digit += ''
+#     else:
+#         digit += i
+# print(digit)
+
+# arf = input()
+# num = ''
+# cnt = 0
+# for i in arf:
+#     if i != '+':
+#         num += i
+#     else:
+#         cnt += int(num)
+#         num= ''
+# print(cnt+int(num))
+
+# num = input()
+# num = num.split('+')
+# bob = 0
+# for i in num:
+#     bob += int(i)
+#
+# print(bob)
