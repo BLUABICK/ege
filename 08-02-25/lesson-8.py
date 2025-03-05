7a811291f06eb12e2cf6b32cd1b7bc82cb195d72
AMishunov <mishunov612@gmail.com>
2025-03-05 17:18:08 +0200

    101001

--- 08-02-25/lesson-8.py
+++ 08-02-25/lesson-8.py
@@ -1,36 +1,36 @@
-#
-# import random
-# sp_sok=random.sample(range(1, 10000),100)
-# max1=sp_sok[0]
-# max2 = 0
-# print(sp_sok)
-# print(sorted(sp_sok)[-2:][::-1])
-# # for i in range(0, len(sp_sok)):
-# #     if sp_sok[i]>max1:
-# #         max1=sp_sok[i]
-# # for i in range(0, len(sp_sok)):
-# #     if sp_sok[i] != max1 and sp_sok[i]>max2:
-# #         max2=sp_sok[i]
-# #
-# # print(max1, max2)
-# # print(sorted(sp_sok)[-2:][::-1])
-# maxx = [0,0]
-# for i in range(len(sp_sok)):
-#     if maxx[0] < sp_sok[i]:
-#         maxx[1] = maxx[0]
-#         maxx[0] = sp_sok[i]
-#     elif maxx[1]<sp_sok[i] and maxx[1]<maxx[0]:
-#         maxx[1]=sp_sok[i]
-# print(maxx)
+
+import random
+sp_sok=random.sample(range(1, 10000),100)
+max1=sp_sok[0]
+max2 = 0
+print(sp_sok)
+print(sorted(sp_sok)[-2:][::-1])
+for i in range(0, len(sp_sok)):
+    if sp_sok[i]>max1:
+        max1=sp_sok[i]
+for i in range(0, len(sp_sok)):
+    if sp_sok[i] != max1 and sp_sok[i]>max2:
+        max2=sp_sok[i]
+
+print(max1, max2)
+print(sorted(sp_sok)[-2:][::-1])
+maxx = [0,0]
+for i in range(len(sp_sok)):
+    if maxx[0] < sp_sok[i]:
+        maxx[1] = maxx[0]
+        maxx[0] = sp_sok[i]
+    elif maxx[1]<sp_sok[i] and maxx[1]<maxx[0]:
+        maxx[1]=sp_sok[i]
+print(maxx)
 from itertools import count
-# import random
-# sp_sok=random.sample(range(1, 10000),1000)
-# print(sp_sok)
-# count = 0
-# for i in range(len(sp_sok)):
-#     if  sp_sok[i] == sp_sok[]:
-#         count +=1
-#     print(count)
+import random
+sp_sok=random.sample(range(1, 10000),1000)
+print(sp_sok)
+count = 0
+for i in range(len(sp_sok)):
+    if  sp_sok[i] == sp_sok[0]:
+        count +=1
+    print(count)
 
 from random import *
 len_n = 100
---
+++ 26-02-25/lesson 13.py
@@ -0,0 +1,9 @@
+
+text = input('Введите строку')
+old = input('Введите что заменить')
+new = input('Введите на что заменить')
+
+if old in text:
+
+else:
+    print('нет такого')
