8b5da7108490f708985a794bede3185f714e7b94
AMishunov <mishunov612@gmail.com>
2025-04-19 12:46:28 +0200

    задача 26-4956

---
+++ 19-04-25/TASK 26-7274.py
@@ -0,0 +1,18 @@
+with open('26_7274.txt') as file:
+    N = int(file.readline())
+    trees = []
+    for i in file:
+        row, col = map(int, i.split())
+        trees.append([row,col])
+trees.sort()
+print(trees)
+cnt = 0
+bob = []
+for i in range(len(trees)-1):
+    if trees[i][0] == trees[i+1][0]:
+        if trees[i+1][1] - trees[i][1] - 1 == 13:
+            bob.append(i)
+print(bob, trees[bob[-1]][0],trees[bob[-1]][1]+1)
+
+
+
