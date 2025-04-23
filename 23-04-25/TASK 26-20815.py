9dc2f1c41454fe04d0f8819573fca299a8f28178
AMishunov <mishunov612@gmail.com>
2025-04-23 17:47:32 +0200

    задача 26-20815

---
+++ 23-04-25/TASK 26-20815.py
@@ -0,0 +1,10 @@
+with open('26_20815.txt') as file:
+    N, K = map(int, file.readline().split())
+    res = []
+    for i in file:
+        res.append(list(map(int, i.split())))
+
+res.sort(key=lambda arr: (-(arr[1]+arr[2]+arr[3]+arr[4]), -arr[4], arr[0]))
+print(res)
+for i in range(len(res)):
+    if res[i]
