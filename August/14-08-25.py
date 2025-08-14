2672e09bffd5a18bc49b3a09cb8e768a928001a0
Supermegamozg2008 <mishunov612@gmail.com>
2025-08-14 17:41:37 +0200

    регулярные выражения задачи

---
+++ August/14-08-25.py
@@ -0,0 +1,23 @@
+from re import *
+
+# with open('2405.txt') as file:
+#     text = file.readline()
+#
+# pattern = r'((KLMN)?(KLMN)*)|((LMN)?(KLMN)*)|((MN)?(KLMN)*)|((N)?(KLMN)*)'
+# matches = [match.group() for match in finditer(pattern, text)]
+# print(len(max(matches)))
+
+# with open('2416.txt') as file:
+#     text = file.readline()
+# pattern = r'([AE][BCD])+'
+# matches = [match.group() for match in finditer(pattern, text)]
+# print(matches)
+# print(len(max(matches, key=len)) // 2)
+
+with open('2402.txt') as file:
+    text = file.readline()
+number = r'([789][0789]*|0)'
+pattern = fr'({number}[\*\-])+{number}'
+matches = [match.group() for match in finditer(pattern, text)]
+print(matches)
+print(len(max(matches, key=len)))
