813d7062475090ab7cea4de27f72c6e0d818c8dd
BLUABICK <mishunov612@gmail.com>
2025-11-05 20:21:20 +0200

    дз на 06.11(https://kompege.ru/variant?kim=25122571)

--- homework/06-11-25/task-24266.py
+++ homework/06-11-25/task-24266.py
@@ -3,22 +3,21 @@ from turtle import *
 screensize(3000,3000)
 tracer(0)
 m = 10
-for x1 in range(100):
-    for i in range(4):
-        fd(x1*m)
-        rt(90)
-        fd(48*m)
-        rt(90)
+for i in range(4):
+    fd(30*m)
+    rt(90)
+    fd(48*m)
+    rt(90)
 penup()
 fd(27*m)
 right(90)
 fd(24*m)
-rt(90)
+lt(90)
 pendown()
 for i in range(4):
     fd(29*m)
     rt(90)
-    fd(18*m)
+    bk(18*m)
     rt(90)
 
 penup()
@@ -27,7 +26,7 @@ for x in range(-50,70):
         goto(x*m, y*m)
         dot(3,'red')
 
-#Блин я хз как тут сделать я не пойму
-
+print((18+3)*2)
+#x=30
 update()
 done()
