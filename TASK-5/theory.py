b7cff6e7c58fea368067264aa7f725b0ca8cd6e3
Supermegamozg2008 <mishunov612@gmail.com>
2025-11-10 17:10:41 +0200

    ТЕОРИЯ %

--- TASK-5/theory.py
+++ TASK-5/theory.py
@@ -37,3 +37,16 @@ tri_num = '1201'
 print(int(bin_num,2))
 print(int(oct_num,8))
 print(int(tri_num,3))
+
+# сумма цифр числа
+# двоичное число
+R_1 = '101'
+sum_1 = R_1.count('1')
+
+# число в любой системе до 10 включительно
+R_2 = '198'
+sum_2 = sum(map(int,R_2))
+
+# число в любой системе до 10 включительно
+R_3 = 'AF7'
+sum_3 = sum(map(lambda x: int(x,36), R_3))
