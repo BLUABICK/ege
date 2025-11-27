e9203908ad3ace9f1e3c01fd3c73eed4fc76b609
Supermegamozg2008 <mishunov612@gmail.com>
2025-11-27 16:29:22 +0200

    дз репак

--- homework/27-11-25/task-21261.py
+++ homework/27-11-25/task-21261.py
@@ -8,11 +8,12 @@ ans = []
 for N in range(10000):
     R = convert(N, 4)
     if sum(map(int, R))%3==0:
-       R = '32' + R.replace('0', '.').replace('2', '0').replace('.', '2')
+        R = '32' + R.replace('0', '.').replace('2', '0').replace('.', '2')
     else:
-        for i in range(len(R)):
-            R = R[0] + '10' + R[3:] + '33'
+        R = R[0] + '10' + R[3:] + '33'
     R = int(R,4)
-
-# ну тут я не пойму что дальше
+    if R>320:
+        ans.append([R,N])
+ans.sort(key=lambda x:(x[0], -x[1]))
+print(ans[0][1])
 
