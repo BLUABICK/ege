272d14e095afbbca69a5f53b0a99bf8989cf6e46
Supermegamozg2008 <mishunov612@gmail.com>
2025-12-11 15:55:45 +0200

    дз доделал

--- homework/11-12-25/task-6261.py
+++ homework/11-12-25/task-6261.py
@@ -4,11 +4,12 @@ alph = printable[:8]
 cnt = 0
 for val in product(alph, repeat=10):
     val = ''.join(val)
-    for i in printable[1:8:2]:
-        val = val.replace(i, '*')
-    if val[0]!='0' and val.count('7')==5 and '*7' not in val and '7*' not in val:
-        cnt += 1
+    if val[0] != '0' and val.count('7') == 5:
+        for i in printable[1:6:2]:
+            val = val.replace(i, '*')
+        if '*7' not in val and '7*' not in val and '77' not in val:
+            cnt += 1
 print(cnt)
-# тут я тупой, yt пойму он слишком долго работает
+
 
 
