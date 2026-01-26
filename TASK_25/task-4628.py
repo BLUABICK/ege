1bf401f3ceda67779eeaba4057c9c622aef72d3a
Supermegamozg2008 <mishunov612@gmail.com>
2026-01-26 17:26:30 +0200

    25 задание с масками (https://kompege.ru/variant?kim=25147561)

---
+++ TASK_25/task-4628.py
@@ -0,0 +1,6 @@
+from fnmatch import fnmatch
+
+
+for N in range(161, 10**8+1, 161):
+    if fnmatch(str(N), '12*4?65'):
+        print(N, N//161)
---
+++ TASK_25/task-9754.py
@@ -0,0 +1,6 @@
+from fnmatch import fnmatch
+
+
+for N in range(2023, 10**8+1, 2023):
+    if fnmatch(str(N), '3?1*57'):
+        print(N, N//2023)
