e986c17b62f922b25ebcaf61e7a0a9409fc0dd72
Supermegamozg2008 <mishunov612@gmail.com>
2026-02-19 16:13:10 +0200

    hw

--- homework/19-02-26/task-10921.py
+++ homework/19-02-26/task-10921.py
@@ -1,7 +1,8 @@
 from itertools import *
-cnt=0
+
+cnt = 0
 for val in set(permutations('ДЖАВАСКРИПТ')):
     val = ''.join(val)
+    if val.find('А') + val.index('И') + val.rfind('А') + 3 == 11:
+        cnt += 1
 print(cnt)
-
-#чего
