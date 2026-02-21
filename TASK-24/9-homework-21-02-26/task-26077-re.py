403e85d8ed42537a2c6d0f05a1c33157338a8368
BLUABICK <mishunov612@gmail.com>
2026-02-21 15:13:46 +0200

    HW

---
+++ TASK-24/9-homework-21-02-26/task-26077-re.py
@@ -0,0 +1,10 @@
+from re import finditer
+
+
+with open(r'..\files\24_26077.txt') as file:
+    data = file.readline()
+
+pattern = r'G([^13579G]*[13579]){45}[^13579G]*'
+matches = [match.group() for match in finditer(pattern, data)]
+
+print(len(max(matches, key=len)))
