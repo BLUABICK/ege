bff1c7087bfd103be2df086d0cfdfe44a47ce8cf
BLUABICK <mishunov612@gmail.com>
2026-02-28 16:32:44 +0200

    24

---
+++ TASK-24/11-practice-280226/pol-task-8834.py
@@ -0,0 +1,7 @@
+from re import finditer
+with open(r'..\files\24-371.txt') as file:
+    data = file.readline()
+
+pattern = r'(A[B-Z ]*){98}\.'
+matches = [match.group() for match in finditer(pattern, data)]
+print(len(min(matches, key=len)))
