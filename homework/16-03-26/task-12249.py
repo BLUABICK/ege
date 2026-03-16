5e1c5611658d4d37f572bb121c78bfc38c1d33d4
Supermegamozg2008 <mishunov612@gmail.com>
2026-03-16 16:02:53 +0200

    hw

--- homework/16-03-26/task-12249.py
+++ homework/16-03-26/task-12249.py
@@ -1,14 +1,13 @@
 with open(r'.\17_12249.txt') as file:
     data = [int(i) for i in file]
 
+maxx = max([x for x in data if 10000<=abs(x)<=99999 and abs(x)%10==3])
 
 ans = []
 for num1, num2, num3 in zip(data, data[1:], data[2:]):
-    maxx = max(data)
-    if len(str(maxx))==5 and maxx%10==3:
-        u1 = abs(num1)%10==3 or abs(num2)%10==3 or abs(num3)%10==3
-        u2 = num1 + num2 + num3 <= maxx
-        if u1 + u2 >= 1:
-            ans.append(num1 + num2 + num3)
+    u1 = abs(num1)%10==3 or abs(num2)%10==3 or abs(num3)%10==3
+    u2 = num1 + num2 + num3 <= maxx
+    if u1 + u2 == 2:
+        ans.append(num1 + num2 + num3)
 
 print(len(ans), max(ans))
