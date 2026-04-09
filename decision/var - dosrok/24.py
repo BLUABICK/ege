2055a80ebe173c5907ac1f99c8a6e16378c8b99e
BLUABICK <mishunov612@gmail.com>
2026-04-09 21:33:01 +0200

    досрок

--- decision/var - dosrok/24.py
+++ decision/var - dosrok/24.py
@@ -2,9 +2,9 @@ with open('24_28765.txt') as file:
     data = file.readline()
 
 data = data.split('BC')
-data = data
 ans = []
-for i in range(len(data)- 180 - 1):
-    line = 'BC'.join(data[i:i + 180-1])
-    ans.append(len(line))
+
+for i in range(len(data) - 180):
+    line = 'BC'.join(data[i:i + 181])
+    ans.append(len(line) + 2)
 print(max(ans))
