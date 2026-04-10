c975790a3b3a27196373f59f7623e5f6ca71ddf9
BLUABICK <mishunov612@gmail.com>
2026-04-10 21:00:18 +0200

    досрок

---
+++ silly/test.py
@@ -0,0 +1,28 @@
+from turtle import *
+tracer(0)
+
+
+
+
+
+# # angle = 91
+# # showturtle()
+# # shape("turtle")
+# # for x in range(100):
+# #     circle(x)
+# #     left(angle)
+#
+# showturtle()
+# shape("turtle")
+# pencolor('purple')
+# for x in range(100):
+#         forward(200)
+#         left(175)
+#
+#
+#
+# done()
+
+
+
+
