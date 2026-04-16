2d2abb27bb6ea3a47bbc746084a15b4025c8cb9a
Supermegamozg2008 <mishunov612@gmail.com>
2026-04-16 16:30:58 +0200

    HW

--- homework/16-04-26/task-28710.py
+++ homework/16-04-26/task-28710.py
@@ -1,11 +1,3 @@
-def is_prime(num):
-    if num < 2:
-        return False
-    for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
-
 def fact(num):
     d = []
     while num%2 ==0:
@@ -13,19 +5,18 @@ def fact(num):
         num//=2
     i = 3
     while i*i <num+1:
-        while num%i==0 and is_prime(num%i):
+        while num%i==0:
             d+=[i]
             num//=i
         i+=2
-    if num>2 and is_prime(num):
+    if num>2:
         d+=[num]
-    if len(d)==3 and 5 in d and 3 in d:
-        return max(d)
-    return 0
+    return d
+
 cnt = 0
-for N in range(3600000, 10**10):
+for N in range(3600001, 10**10):
     M = fact(N)
-    if M:
+    if len(M)==3 and all('3' in str(m) and '5' in str(m) for m in M):
         print(N, M)
         cnt+=1
         if cnt==5:
