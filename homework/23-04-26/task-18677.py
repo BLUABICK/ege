82b6907783c410b326e37ae3825c44e2ec9c6450
BLUABICK <mishunov612@gmail.com>
2026-04-23 14:14:06 +0200

    HW

---
+++ homework/23-04-26/task-18677.py
@@ -0,0 +1,30 @@
+from math import dist
+
+def center(cluster):
+    res = []
+    for dot in cluster:
+        sum_dist = sum(dist(dot, d) for d in cluster)
+        res.append([sum_dist, dot])
+    return min(res)[1]
+
+
+with open('27B_18677.txt') as file:
+    dots = [list(map(float, i.replace(',', '.').split())) for i in file]
+
+eps = 1
+clusters = []
+while dots:
+    cluster = [dots.pop()]
+    for dot in cluster:
+        for d in dots.copy():
+            if dist(dot, d) < eps:
+                cluster.append(d)
+                dots.remove(d)
+    if len(cluster)>30:
+        clusters.append(cluster)
+
+centers = [center(cluster) for cluster in clusters]
+print([len(cluster) for cluster in clusters])
+print(centers)
+print((abs(centers[0][0]+centers[1][0]+centers[2][0])/3)*100000)
+print((abs(centers[0][1]+centers[1][1]+centers[2][1])/3)*100000)
---
+++ homework/23-04-26/task-27138.py
@@ -0,0 +1,34 @@
+from math import dist
+
+def center(cluster):
+    res = []
+    for dot in cluster:
+        sum_dist = sum(dist(dot, d) for d in cluster)
+        res.append([sum_dist, dot])
+    return min(res)[1]
+
+
+with open('27A_27138.txt') as file:
+    dots = [list(map(float, i.replace(',', '.').split())) for i in file]
+
+eps = 1
+clusters = []
+while dots:
+    cluster = [dots.pop()]
+    for dot in cluster:
+        for d in dots.copy():
+            if dist(dot, d) < eps:
+                cluster.append(d)
+                dots.remove(d)
+    if len(cluster)>30:
+        clusters.append(cluster)
+
+centers = [center(cluster) for cluster in clusters]
+M_C = max(cluster for cluster in clusters)
+print(M_C)
+print([len(cluster) for cluster in clusters])
+print(centers)
+# print(abs(centers[0][0]-centers[1][0])*10000)
+# print(abs(centers[0][1]-centers[1][1])*10000)
+print(abs(centers[0][0]-centers[1][0])*10000)
+print(abs(centers[0][1]-centers[1][1])*10000)
