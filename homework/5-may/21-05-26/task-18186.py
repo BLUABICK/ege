e27501931328a4014194feb118ef538dda4d06bc
Supermegamozg2008 <mishunov612@gmail.com>
2026-05-21 16:13:43 +0200

    24

--- homework/5-may/21-05-26/task-18186.py
+++ homework/5-may/21-05-26/task-18186.py
@@ -2,8 +2,8 @@ from re import *
 with open('24_18186.txt') as file:
     data = file.readline()
 bukvs = r'[BCDFGH][BCDFGH][AE]'
-pattern = fr'(?<={bukvs})(([BCDFGH]*[AE]+[BCDFGH]+[AE]+)|([AE]+[BCDFGH]*[AE]*))(?={bukvs})'
+pattern = fr'(?<={bukvs})(([BCDFGH]*[AE]+[BCDFGH]+)|([AE]+[BCDFGH]*[AE]*))(?={bukvs})'
 
 matches = [match.group() for match in finditer(pattern, data)]
 print(matches)
-print(len(max(matches, key=len)))
+print(len(max(matches, key=len))+6)
--- homework/5-may/21-05-26/task-18239.py
+++ homework/5-may/21-05-26/task-18239.py
@@ -1,8 +1,18 @@
 from re import *
-with open('24_18239.txt') as file:
+
+with open(r'24_18239.txt') as file:
     data = file.readline()
 
-pattern = r'([-][1-9]+)*'
+num = r'(0|[1-9][0-9]*)'
+max_len = 0
 
-matches = [match.group() for match in finditer(pattern, data)]
-print(matches)
+for i in range(len(data)):
+    for j in range(i + 1, len(data) + 1):
+        sub = data[i:j]
+        if match(fr'^-?{num}(?:-{num})*$', sub):
+            nums = [int(x) for x in findall(r'-?\d+', sub)]
+            val = nums[0] - sum(nums[1:])
+            if val > -20000:
+                max_len = max(max_len, j - i)
+
+print(max_len)
