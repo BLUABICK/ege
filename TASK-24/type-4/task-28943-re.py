f64436719ce433cbcfaa9875128925dc0e206f41
Supermegamozg2008 <mishunov612@gmail.com>
2026-05-25 17:45:13 +0200

    24

---
+++ TASK-24/type-4/task-28943-re.py
@@ -0,0 +1,9 @@
+from re import *
+with open(r'../files/24_28943.txt') as file:
+    data = file.readline()
+data = data.replace('20', '--')
+pattern = r'(--[^AEIOUY-]*){26}[AEIOUY]'
+
+matches = [match.group() for match in finditer(pattern, data)]
+
+print(len(min(matches,key=len)))
