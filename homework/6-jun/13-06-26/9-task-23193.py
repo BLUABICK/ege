183076664e9a3576e65abd2a71602fd63b6287d0
Supermegamozg2008 <mishunov612@gmail.com>
2026-06-13 11:19:01 +0200

    26

--- homework/6-jun/13-06-26/9-task-23193.py
+++ homework/6-jun/13-06-26/9-task-23193.py
@@ -4,7 +4,7 @@ with open('9_23193.txt') as file:
 
 for i in data:
     i2 = sorted(i, key=lambda x:i.count(x), reverse=True)
-    u1 = len(set(i2))==4
+    u1 = i2.count(i[0])==3 and i2.count(i[-1])==1 and i2.count(i[-2])==1 and i2.count(i[-3])==1
     u2 = sum(i2[:3])/3 > sum(i2[3:])/3
     if u1+u2==2:
         print(data.index(i)+1)
