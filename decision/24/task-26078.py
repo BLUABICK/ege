f0aa283d3c6ccbc3e2a77069352f5c22664ed535
Supermegamozg2008 <mishunov612@gmail.com>
2026-06-17 14:20:58 +0200

    f

--- decision/24/task-26078.py
+++ decision/24/task-26078.py
@@ -1,11 +1,11 @@
 with open('24_26078.txt') as file:
     data = file.readline()
-'W*****W****W******W'
+'***W*****W****W******W****W******W'
 data = data.split('W')
 
 ans = []
-for i in range(1, len(data)-91):
-    line = 'W'.join(data[i:i+89])
+for i in range(1, len(data)-89):
+    line = 'W' + 'W'.join(data[i:i+89]) + 'W'
     if line.count('2025')>=110:
         ans.append(line)
 print(len(min(ans, key=len)))
